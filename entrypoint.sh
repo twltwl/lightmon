2252c6dd0951feac2e24126baab9af9b993e7ff6
Tommy *twl* Ljungberg <tommy.ljungberg@bbm.bonnier.se>
2018-03-19 17:06:57 +0100

    setting chrome flags correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 if [ -z "$1" ]; then
   npm run start
 else
-  lighthouse --port=9222 --chrome-flags="--headless" --output-path=stdout  --disable-setuid-sandbox --no-sandbox $@
+  lighthouse --port=9222 --chrome-flags="--headless  --disable-gpu --disable-setuid-sandbox --no-sandbox" --output-path=stdout $@
 fi
